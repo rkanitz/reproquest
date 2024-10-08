3b0dc8517515e652d9d0f203015e6a9c8b392de0
Ricardo Kanitz <ricardo.kanitz@syngenta.com>
2024-10-08 16:58:48 +0200

    Update qr codes again :-/

--- create_qrcode.R
+++ create_qrcode.R
@@ -3,10 +3,12 @@ library(qrcode)
 
 
 # Define the URL
-my_url <- "https://rkanitz.shinyapps.io/reproquest/"
+my_url_survey <- "https://s.surveyplanet.com/ef64jql9"
+my_url_result <- "https://rkanitz.shinyapps.io/reproquest_results/"
 
 # Generate the QR code
 qr_out_survey <- qr_code(my_url_survey)
 qr_out_result <- qr_code(my_url_result)
 plot(qr_out_survey)
 plot(qr_out_result)
+plot(qr_out_survey)
